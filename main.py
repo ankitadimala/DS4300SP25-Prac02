c1f6aaf3b7ca63ae423dba64e4a4ab9cce210be1
Phisomni <94452189+Phisomni@users.noreply.github.com>
2025-03-20 18:30:13 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import preprocess
+import indexing
+
+def main():
+    print("Extracting slides...")
+    preprocess.process_pdfs("data/")
+    print("Indexing slides in Redis, ChromaDB, and FAISS...")
+    indexing.create_hnsw_index()
+    indexing.process_and_store()
+    print("System ready for querying.")
+
+if __name__ == "__main__":
+    main()
