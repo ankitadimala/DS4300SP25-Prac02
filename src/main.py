3b1715b00a59d68816592bb49f5f772131dd9d59
ankitadimala <106349773+ankitadimala@users.noreply.github.com>
2025-03-23 16:30:29 -0400

    Updated main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 import os
+import time
 import json
 import preprocessing, chunking, indexing
 
 
 def main():
+    start_time = time.time()
+
     print("Preprocessing PDFs...")
     raw_data = preprocessing.process_folder("data")
 
+    print("Chunking text...")
     all_chunks = []
     for filename, text in raw_data.items():
         module_name = os.path.splitext(filename)[0]
@@ -27,8 +31,11 @@ def main():
     print("Creating Redis index and storing embeddings...")
     indexing.create_hnsw_index()
     indexing.process_and_store()
+    
     print("Indexing complete. Ready for querying!")
 
+    end_time = time.time()
+    print(f"\n Pipeline completed in {end_time - start_time:.2f} seconds.")
 
 if __name__ == "__main__":
     main()
